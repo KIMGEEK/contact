b8af9fc4f05cc36eb53252d02eef0e2ac0cbd365
LAPTOP-MLL80S8J\kgeek <baekhajun@chungbuk.ac.kr>
2022-05-09 15:32:12 +0900

    lint 경고 메시지 생략 커맨드 입력, Column&Row와 정렬 기능 연습

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,9 +11,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(),
-      body: Container(),
-      bottomNavigationBar: BottomAppBar(),
-    ));
+            appBar: AppBar(),
+            body: Container(
+              color: Colors.grey,
+              height: 700,
+              width: 500,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.pink,
+                      size: 24.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.pink,
+                      size: 24.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.pink,
+                      size: 24.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
+                    ),
+                  ]),
+            )));
   }
 }
